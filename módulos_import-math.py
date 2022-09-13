e3864a6363d93ea35af56ee2b88a1db1fd75f36c
zyonzarhaf <zyonzarhaf@gmail.com>
2022-09-13 15:16:34 -0700

    módulos_import-math.py

---
+++ módulos_import-math.py
@@ -0,0 +1,28 @@
+import math
+#mostra a raiz quadrada de um número
+n1 = int(input('Digite um número: '))
+r = math.sqrt(n1)
+print('A raiz de {} é igual a {:.2f}'.format(n1, r))
+#mostra um número real truncado
+n2 = float(input('Digite um valor real qualquer: '))
+i = math.trunc(n2)
+print('A porção inteira de {} é {}'.format(n2, i))
+#mostra o comprimento da hipotenusa de um triângulo com base em seus catetos oposto e adjacente
+cat_o = int(input('Informe o valor do cateto oposto: '))
+cat_a= int(input('Agora informe o valor do cateto adjacente: '))
+hipo = math.hypot(cat_o, cat_a)
+print('A hipotenusa vale {}.'.format(math.trunc(hipo)))
+#mostra o valor do seno, do cosseno e da tangente de um ângulo qualquer
+n3 = int(input('Informe um ângulo qualquer: '))
+s = math.sin(n3)
+c = math.cos(n3)
+t = math.tan(n3)
+print('O seno de {} é {:.2f}, o coseno é {:.2f} e a tangente é {:.2f}.'.format(n3, (s), (c), (t)))
+#sorteia um nome
+import random
+a1 = str(input('Digite o nome do 1o aluno: '))
+a2 = str(input('Digite o nome do 2o aluno: '))
+a3 = str(input('Digite o nome do 3o aluno: '))
+a4 = str(input('Digite o nome do 4o aluno: '))
+a5 = str(input('Digite o nome do 5o aluno: '))
+print('O aluno {} foi escolhido para apagar a lousa.'.format())
